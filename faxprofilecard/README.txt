3ebf9f2169f6698fa4dc01ebbb719a3c05789ab5
Dblosser635 <88632498+Dblosser635@users.noreply.github.com>
2022-03-20 11:20:22 -0400

    Add files via upload

---
+++ faxprofilecard/README.txt
@@ -0,0 +1,3 @@
+See config.js for all the config options for FaxProfileCard.
+
+To edit the background or profile image replace the files in /assets/images
---
+++ faxprofilecard/config.js
@@ -0,0 +1,21 @@
+// Site Settings //
+
+var siteTitle = `My Profile Card Site`;
+var siteDescription = "Everything you see can be edited! This is a demo site and is a bought product. Click the globe icon below for more!";
+var buttonColor = "#FFFFFF";
+var buttonHoverColor = "#1d1d1d";
+
+var firstBoxGradColor = "#FFDB25";
+var secondBoxGradColor = "#FA8C00";
+
+// Buttons //
+// Isons can range from anything in /assets/icons.svg. Like youtube, discord, twitter, github, globe etc.
+var buttonsArray = [
+    // Format: "LINK::Icon"
+    // "http://example.com::twitter"
+    "https://faxes.zone/viewproduct/?id=16::globe",
+    "https://twitter.com/FAXES_YT::twitter",
+    "https://faxes.zone/youtube::youtube",
+    "http://example.com::email",
+    "http://example.com::email",
+]
